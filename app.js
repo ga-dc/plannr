aee2e830105affb20711c9f15a5c460601e88fd4
Rachel Warren <raelwa@gmail.com>
2015-09-03 19:27:27 -0400

    form and map showing

---
+++ app.js
@@ -0,0 +1,16 @@
+var map = L.map("map").setView([38.977554,-78.255478], 15);
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    id: 'rewarren17.45dfc1d7',
+    accessToken: 'pk.eyJ1IjoicmV3YXJyZW4xNyIsImEiOiI5ODRmNmRhNmRmYmViNjY1NzJlNThhZGQ2ZTE5MGQ4MCJ9.BZwVN4HQhbQtXtuHRxmm9A',
+    maxZoom: 18
+}).addTo(map);
+
+var redhawk = L.marker([38.977554,-78.255478]).addTo(map);
+
+redhawk.bindPopup("<h3>HOME</h3>");
+
+
+$("#submit").on("click", function(){
+  alert("Thanks!");
+});
