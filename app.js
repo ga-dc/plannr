72c03711c4de053c06f95cb94c22bb2c6092059c
Omari Mayers-Walker <omari@fastmail.fm>
2015-09-04 08:21:01 -0400

    added ajax call

--- app.js
+++ app.js
@@ -13,4 +13,15 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
 $("#location").on("submit", function(event){
     event.preventDefault()
     console.log(event)
+    var searchLocal = $("#search").val().replace(/ /g, "+")
+    $.ajax({
+        url: "http://api.opencagedata.com/geocode/v1/json?query=" + searchTerm + "&pretty=1&key=#####"
+    }).done(function(res){
+       var response = res.results[0]
+
+        var lat = response.geometry.lat
+        var lng = response.geometry.lng
+        var name = response.components.attraction || response.components.building
+
+    })
 })
