f2c19852e754f06245988e3a82f0a2fbef332d9c
Lisa <lscottcook@hotmail.com>
2015-09-03 21:29:19 -0400

    edited form

--- app.js
+++ app.js
@@ -10,3 +10,48 @@ var control = L.Control.openCageSearch(options).addTo(map);
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '© OpenStreetMap contributors'
 }).addTo(map);
+
+map.on( "click", function( event ){
+  console.log( event );
+})
+
+var clickPopup = L.popup();
+
+// Now define when and how `clickPopup` will appear.
+map.on( "click", function( event ){
+  clickPopup
+  .setLatLng( event.latlng )
+  .setContent( "Coordinates: " + event.latlng.toString() )
+  .openOn( map );
+})
+
+// var geocoder;
+// $(document).ready(function() {
+//     geocoder = new OpenStreetMap.maps.Geocoder();
+//     $('form.geocode').submit(function(e) {
+//         var that = this;
+//         var addr;
+//         var addrArray = [];
+//         var addrFields = ['search'];
+//         $(addrFields).each(function(idx, name) {
+//             var val = $(this).find('input[name="' + name + '"]').val();
+//             if (val.length) {
+//                 addrArray.push(val);
+//             }
+//         });
+//         if (addrArray.length) {
+//             e.preventDefault();
+//             $(that).unbind('submit');
+//             var onSuccess = function(results, status) {
+//                 if (status == google.maps.GeocoderStatus.OK) {
+//                     result = results[0].geometry.location;
+//                     var point = result.lat() + ', ' + result.lng();
+//                     $(that).prepend('<input type="hidden" name="point" value="' + point + '">');
+//                 }
+//                 $(that).trigger('submit');
+//             }
+//             addr = addrArray.join(', ');
+//             geocoder.geocode({'address': addr}, onSuccess);
+//         }
+//     });
+// });
