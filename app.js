0e10d1745a721e5c1cb64eb486d0daf95c59a675
Sarah Hersh <schersh@gmail.com>
2016-01-07 20:25:53 -0500

    added app.js functions

--- app.js
+++ app.js
@@ -1,14 +1,33 @@
-var makeMarker = function(geoInfo){
-  var lat = event.latlng.lat,
-      lng = event.latlng.lng;
-  var marker = L.marker([geoInfo).addTo(myMap);
-  marker.bindPopup("<p>" + event.latlng.lat + "</p><p>" + event.latlng.lng + "</p>");
-}
+var Map = function(){
+  self = this;
 
-myMap.on( "click", function( event ){
+  $("#submit").on("click", function (event){
+    event.preventDefault();
+    var address = $("#address").val();
+    $.ajax({
+    url: "https://api.opencagedata.com/geocode/v1/json?query" + address + 'pk.eyJ1Ijoic2NoZXJzaCIsImEiOiJjaWozOG5ucTUwMDQwdHVrcGFkbG84ZTZiIn0.uaY3C7_AYcLYEfjoU8EVsw'
+  }).done(function(res){
+    response = res.results[0];
+  });
+    var lat = event.latlng.lat;
+    var lng = event.latlng.lng;
+    var place = response.components.attraction || response.components.building
+  });
+};
 
-  var lat = results.location.lat.val()
-  var lng = results.location.lng.val()
-  var geoInfo = "[" + lat "," + lng + "]"
-  makeMarker(geoInfo)
-});
+this.renderMap = function(){
+  var map = L.map('map').setView([38.895008, -76.992288], 15);
+
+  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    mazZoom: 18,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    id: 'schersh.okh88oae',
+    accessToken: 'pk.eyJ1Ijoic2NoZXJzaCIsImEiOiJjaWozOG5ucTUwMDQwdHVrcGFkbG84ZTZiIn0.uaY3C7_AYcLYEfjoU8EVsw'
+  }).addTo(map);
+  return map;
+};
+
+this.makeMarker = function(lat, lng, place){
+  var marker = L.marker([ lat, lng ]).addTo(map);
+  marker.bindPopup("<p>" + place + "</p>");
+};
