385ebaa03e612f1a537d9657eeaf7263c37917c4
Sarah Hersh <schersh@gmail.com>
2016-01-07 20:42:02 -0500

    added map functions to app.js

--- app.js
+++ app.js
@@ -1,21 +1,4 @@
-var Map = function(){
-  self = this;
-
-  $("#submit").on("click", function (event){
-    event.preventDefault();
-    var address = $("#address").val();
-    $.ajax({
-    url: "https://api.opencagedata.com/geocode/v1/json?query" + address + 'pk.eyJ1Ijoic2NoZXJzaCIsImEiOiJjaWozOG5ucTUwMDQwdHVrcGFkbG84ZTZiIn0.uaY3C7_AYcLYEfjoU8EVsw'
-  }).done(function(res){
-    response = res.results[0];
-  });
-    var lat = event.latlng.lat;
-    var lng = event.latlng.lng;
-    var place = response.components.attraction || response.components.building
-  });
-};
-
-this.renderMap = function(){
+var renderMap = function(){
   var map = L.map('map').setView([38.895008, -76.992288], 15);
 
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
@@ -27,7 +10,24 @@ this.renderMap = function(){
   return map;
 };
 
-this.makeMarker = function(lat, lng, place){
+var makeMarker = function(lat, lng, place){
   var marker = L.marker([ lat, lng ]).addTo(map);
   marker.bindPopup("<p>" + place + "</p>");
 };
+
+$(document).ready(function(){
+  renderMap();
+  $("#submit").on("click", function (){
+    event.preventDefault();
+    var address = $("#address").val();
+    $.ajax({
+    url: "https://api.opencagedata.com/geocode/v1/json?query" + address + 'pk.eyJ1Ijoic2NoZXJzaCIsImEiOiJjaWozOG5ucTUwMDQwdHVrcGFkbG84ZTZiIn0.uaY3C7_AYcLYEfjoU8EVsw'
+  }).done(function(res){
+    response = res.results[0];
+  });
+    var lat = response.geometry.lat;
+    var lng = response.geometry.lng;
+    var place = response.components.attraction || response.components.building
+    makeMarker(lat, lng, place);
+  });
+});
